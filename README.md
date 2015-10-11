0b08f375c99b0bce649dd0c93b7f42018434d306
Sanjay <91sanjay@gmail.com>
2015-10-10 21:53:31 -0400

    Add source for Word Count Processor
    1. No combiner
    2. Si combiner
    3. Per Map Tally
    4. Per Task Tally

---
+++ README.md
@@ -0,0 +1,12 @@
+# Map Reduce - Northeastern University
+
+## Synopsis
+This repo contains Java Projects to understand and solve problems using Hadoop Map Reduce. The projects have been completed as part of the coursework at Northeastern University
+
+## Installation
+1. Install Java SE 7
+2. Install hadoop 2.6.0
+
+## Contributors
+
+Sanjay Rajashekhar
---
+++ WordCountProcessor/src/com/sanjay/mapreduce/PerMapTally.java
@@ -0,0 +1,109 @@
+package com.sanjay.mapreduce;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class PerMapTally {
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		private Integer one = 1;
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			Map<String, Integer> wordcount = new HashMap<String, Integer>();
+			String lower = "";
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				lower = word.toString().toLowerCase();
+
+				if (lower.startsWith("m") || lower.startsWith("n") || lower.startsWith("o") || lower.startsWith("p")
+						|| lower.startsWith("q")) {
+					if (wordcount.containsKey(word.toString())) {
+						wordcount.put(word.toString(), wordcount.get(word.toString())+1);
+					} else {
+						wordcount.put(word.toString(), one);
+					}
+				}
+			}
+			
+			for (Entry<String, Integer> entry : wordcount.entrySet()) {
+				context.write(new Text(entry.getKey()), new IntWritable(entry.getValue()));
+			}
+		}
+	}
+
+	public static class WordPartitioner extends Partitioner<Text, IntWritable> {
+
+		@Override
+		public int getPartition(Text key, IntWritable value, int numReduceTasks) {
+			String word = key.toString().toLowerCase();
+
+			if (word.startsWith("m")) {
+				return 0;
+			} else if (word.startsWith("n")) {
+				return 1;
+			} else if (word.startsWith("o")) {
+				return 2;
+			} else if (word.startsWith("p")) {
+				return 3;
+			} else if (word.startsWith("q")) {
+				return 4;
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length < 2) {
+			System.err.println("Usage: wordcount <in> [<in>...] <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "word count");
+		job.setJarByClass(PerMapTally.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setPartitionerClass(WordPartitioner.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setNumReduceTasks(5);
+		for (int i = 0; i < otherArgs.length - 1; ++i) {
+			FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+		}
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
